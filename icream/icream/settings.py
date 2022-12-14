c236ab0fac4bc143440999f0deb20e98c9041356
Myshadowss <109855548+Myshadowss@users.noreply.github.com>
2022-12-14 09:30:04 +0530

    static

--- icream/icream/settings.py
+++ icream/icream/settings.py
@@ -156,6 +156,6 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
