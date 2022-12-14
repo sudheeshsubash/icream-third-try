1cfad760f1d8cd24c00e9b948b239917ea2d0f2b
Myshadowss <109855548+Myshadowss@users.noreply.github.com>
2022-12-14 16:50:40 +0530

    debug

--- icream/icream/urls.py
+++ icream/icream/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('order/',include('odershandle.urls')),
 
     
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT,)
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT,)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
