a9372b87cc0dc763321ccb02112682e692de43ee
Myshadowss <109855548+Myshadowss@users.noreply.github.com>
2022-12-16 16:12:18 +0530

    again

--- icream/logins/views.py
+++ icream/logins/views.py
@@ -120,16 +120,15 @@ def otp_validate(request):
     
     if request.method == 'POST':
         user_input_otp = int(request.POST['otp_number'])
-        
-        if int(user_input_otp) == helper.otp_number():
-            
-            user = UserInfo.objects.create_user(username=helper.username,password=helper.password,phone_number = helper.phone,is_block=0)
-            user.save()
-            print(user.id)
-            request.session['username'] = helper.username
-            return redirect('guest_user_home')
-        
-        messages.error(request,'otp is not currect')
+        try:
+            if int(user_input_otp) == helper.otp_number():
+                user = UserInfo.objects.create_user(username=helper.username,password=helper.password,phone_number = helper.phone,is_block=0)
+                user.save()
+                print(user.id)
+                request.session['username'] = helper.username
+                return redirect('guest_user_home')
+        except:
+            messages.error(request,'otp is not currect')
         
     return render(request,'otpgenerate.html')
 
